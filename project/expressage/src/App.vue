0d8c8c267a982ed9e0112665e14fe41018c4d19b
Your Nmae <you@example.com>
2018-11-20 21:41:26 +0800

    快递查询

---
+++ project/expressage/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <h3>快递查询</h3>
+     <ExpressageInput v-on:translateNumber="getData($event)"></ExpressageInput>
+  <hr>
+  <ExpressResult :result="result"></ExpressResult>
+  </div>
+</template>
+
+<script>
+import ExpressageInput from './components/ExpressageInput.vue'
+import ExpressResult from './components/ExpressResult.vue'
+
+
+export default {
+  name: 'app',
+  data:function(){
+    return {
+    //   number:123456,
+      result:null
+    }
+  },
+  components: {
+    ExpressageInput,
+    ExpressResult
+  },
+  // 获取数据 布局  3384524215448   3819618245951
+  methods:{
+    getData:function(n){
+
+      this.axios
+      .get("https://api.apiopen.top/EmailSearch",{
+        params:{
+          number:n
+        }
+      })
+      .then(response=> {
+        this.result=response.data.result;
+        // console.log(response.data.result);
+      })
+        // .catch(function(error) {
+        //   // console.log(error);
+        // });
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ project/expressage/src/components/ExpressageInput.vue
@@ -0,0 +1,14 @@
+<template>
+    <el-input placeholder="请输入快递单号" v-model="number" class="input-with-select" clearable>
+        <el-button slot="append" icon="el-icon-search" @click="$emit('translateNumber', number)"></el-button>
+    </el-input>
+</template>
+<script>
+export default {
+    data:function(){
+        return {
+            number:3383585320902
+        }
+    }
+}
+</script>
